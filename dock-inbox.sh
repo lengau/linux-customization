b2c3a1e0a61a9f2d849b3fc48f43c522306ed224
Alex Lowe <amlowe@ieee.org>
2015-06-08 23:16:10 -0400

    Improved dock-inbox so it also executes Inbox.

--- dock-inbox.sh
+++ dock-inbox.sh
@@ -4,14 +4,14 @@
 # Set this to run in the background when your desktop environment starts and 
 # Inbox can once again join the world of civilised Email apps.
 
-while ! wmctrl -l|grep 'Inbox –'; do
+google-chrome --profile-directory=Default --app-id=pkclgpgponpjmpfokoepglboejdobkpl
+
+while ! wmctrl -l|grep 'Inbox –'|cut -f 1 -d' ';
+do
 	sleep 0.2
 done
 
-inbox_window_id=$(wmctrl -l|grep 'Inbox –'|cut -f 1 -d' ')
+sleep 1
 
-while true; do 
-	kdocker -w $inbox_window_id -i $HOME/.local/share/icons/hicolor/128x128/apps/chrome-pkclgpgponpjmpfokoepglboejdobkpl-Default.png
-	
-	sleep 90
-done
+inbox_window_id=$(wmctrl -l|grep 'Inbox –'|cut -f 1 -d' ')
+kdocker -m -w $inbox_window_id -i $HOME/.local/share/icons/hicolor/128x128/apps/chrome-pkclgpgponpjmpfokoepglboejdobkpl-Default.png &
